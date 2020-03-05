aff07a4f5e2059225205f4497ac91e9cdcc3a4bb
khalender <41184767+khalender@users.noreply.github.com>
2020-03-05 20:00:36 +0100

    Update car-sales.js

--- I.pass-tests/09-car-sales/car-sales.js
+++ I.pass-tests/09-car-sales/car-sales.js
@@ -1,14 +1,17 @@
 function sales(array) {
+    
+    ///find distinct car brands and put them into an array
     distinctBrands = array.map(x => x.make).filter((v, i, a) => a.indexOf(v) == i)
 
     let total = {};
 
-    for (brand of distinctBrands) {
+    
+    for (brand of distinctBrands) { //loop throug distinct car brands
         let key = brand;
-        total[key] = array
-            .filter(y => y.make === brand)
-            .map(p => p.price)
-            .reduce((j, k) => j + k, 0);
+        total[key] = array 
+            .filter(y => y.make === brand) // filter the car brand that is in the current brand
+            .map(p => p.price) // get the price of the object
+            .reduce((j, k) => j + k, 0); // add them up
     }
     return total;
 
